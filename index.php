4ba2bd7087990ffa0f1b9d9a7f1745d409d1811a
Adam <thomasadam83@hotmail.com>
2024-09-03 10:06:20 -0400

    Added vote links

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<?php
+
+$connect = mysqli_connect(
+  "sql213.infinityfree.com",
+  "if0_37236054",
+  "pimn47tQHIKOzk2",
+  "if0_37236054_votes");
+
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -10,6 +19,19 @@
 
     <p>Welcome to the official Web Dev Art Comp 2024. Make your vote below:</p>
 
+    <?php
+
+    $query = 'SELECT * FROM groups';
+    $result = mysqli_query($connect, $query);
+
+    while($record = mysqli_fetch_assoc($result))
+    {
+      echo '<a href="vote.php?id='.$record['id'].'">'.$record['name'].'</a>';
+      echo '<br>';
+    }
+    
+
+    ?>
 
     <hr>
 
